02cc165cb7399a3716fea0a7fbf4dfc5f950b842
bomanaps <mercy.boma35@yahoo.com>
2025-09-19 07:42:22 +0100

    correct error code format

--- cmd/storachafs/root.go
+++ cmd/storachafs/root.go
@@ -18,7 +18,7 @@ to files stored on the decentralized Storacha network.`,
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		fmt.Fprintf(os.Stderr, "Error executing CLI: %v\n", err)
 		os.Exit(1)
 	}
 }
